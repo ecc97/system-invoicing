c1dbd94d38dcf5054fd06510a6fcdf544736b2d0
ecc97 <carmonaedwin1123@gmail.com>
2025-03-16 21:34:09 -0500

    feat: api route by invoice with update and delete operations

---
+++ src/app/api/invoices/[id]/route.ts
@@ -0,0 +1,69 @@
+import { NextResponse } from 'next/server';
+import { InvoiceItem, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function GET(_request: Request, { params }: { params: Promise<{ id: string }> }) {
+    try {
+      const invoiceId = (await params).id;
+      const invoice = await prisma.invoice.findUnique({
+        where: { id: invoiceId },
+        include: { user: true, items: true, payments: true },
+      });
+      if (!invoice) {
+        return NextResponse.json({ error: 'Factura no encontrada' }, { status: 404 });
+      }
+      return NextResponse.json(invoice);
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({ error: 'Error obteniendo la factura' }, { status: 500 });
+    }
+  }
+
+export async function PUT(request: Request, { params }: { params: Promise<{ id: string }> }) {
+  try {
+    const invoiceId = (await params).id;
+    const body = await request.json();
+
+    const { clientName, clientEmail, status, items } = body;
+
+
+    const updatedInvoice = await prisma.invoice.update({
+      where: { id: invoiceId },
+      data: {
+        clientName: clientName,
+        clientEmail: clientEmail,
+        status: status,
+        date: new Date(),
+        items: {
+            deleteMany: {}, 
+            create: items?.map((item: InvoiceItem) => ({
+                description: item.description,
+                rate: item.rate,
+                quantity: item.quantity,
+              })) || [],
+        }
+        
+      },
+      include: { user: true, items: true, payments: true },
+    });
+
+    return NextResponse.json(updatedInvoice);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Error updating invoice' }, { status: 500 });
+  }
+}
+
+export async function DELETE(_request: Request, { params }: { params: Promise<{ id: string }> }) {
+  try {
+    const invoiceId = (await params).id;
+    const deletedInvoice = await prisma.invoice.delete({
+      where: { id: invoiceId },
+    });
+    return NextResponse.json(deletedInvoice);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Error deleting invoice' }, { status: 500 });
+  }
+}
